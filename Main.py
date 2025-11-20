fa4c83f2a75f1b315e17b289799e09d737c99246
vlad <138448988+gearupp@users.noreply.github.com>
2025-11-20 19:53:42 +0000

    Created FileInput + Formater

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
-GRID_SIZE = 12
+import re
+
+grid_size = 11
 global file_location, current_loc
 
 text1 = './Route001.txt'
@@ -20,3 +22,20 @@ S
 X = 0
 Y = 0
 
+def fileinput():
+    while True:
+        global file_location
+        file_location = input("Enter the full path to the route file (or type STOP to exit): ")
+        if re.search("(.txt)",fileinput ):
+            file_formating() # Will run after a path with .txt is provided
+            exit
+        elif file_location.lower().strip() == "stop":
+            print("Exiting Drone simulation.")
+            exit
+
+fileinput()
+
+
+
+def file_formating():
+    print("Running Formatter")
