dfc1efff97c932315259dd2e38a5db90ab642b1e
vlad <138448988+gearupp@users.noreply.github.com>
2025-11-20 10:07:46 +0000

    added info

--- Main.py
+++ Main.py
@@ -2,7 +2,21 @@ GRID_SIZE = 12
 global file_location, current_loc
 
 text1 = './Route001.txt'
-text03
+text03 = './Route003.txt'
+
+'''
+Will use text files above stored locally for testing,
+They look something like this, initial two values are starting position, 
+then subsiquentyly the direction of travel in N E W S directions.
+3
+12
+S
+S
+W
+S
+
+'''
+
 X = 0
 Y = 0
 
