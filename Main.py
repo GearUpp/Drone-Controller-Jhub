82c6d7b38dac24a2803f41fec5abaf6e68fbc0df
GearUpp <138448988+GearUpp@users.noreply.github.com>
2025-11-21 21:27:57 +0000

    Finished Moving but has one bug

--- Main.py
+++ Main.py
@@ -17,75 +17,80 @@ W
 S
 
 '''
-rows = open(text1, "r").read().splitlines()
-print(rows)
-print(int(rows.pop(0)))
+
 x = 0
 y = 0
 
 def fileinput():   # Initial function to read user input and decide next step
     global file_location, rows
+
     while True:
         file_location = str(input("Enter the full path to the route file (or type STOP to exit): "))
-        if re.search(r".txt",str(file_location)):       # change later to file_location
+
+        if re.search(r".txt",str(file_location)):       
             rows = open(file_location, "r").read().splitlines()
             formatting() # Will run after a path with .txt is provided
+
         elif file_location.lower().strip() == "stop":
             print("Exiting Drone simulation.")
             break
+        
     exit   
 
 
 
-def formatting():
+def formatting():   # Will create a starting place from rows list. and confirm its in range of grid + continue to moving fun
     global x,y, current_lococation,file_location,rows
-    try:
-        print(rows)
-        x = int(rows.pop(0))
 
+    try:
+        x = int(rows.pop(0))
         y = int(rows.pop(0))
-        print(rows)
+
         current_lococation = set()
         current_lococation.add((x, y))
-        if y > 12 or x > 12 or y < 0 or x < 0:
+
+        if y > grid_size or x > grid_size or y <= 0 or x <= 0:
             print("Starting coordinates are:  X:", x , " -   Y: ", y  , ". Are not possible and out of range of grid")
             fileinput()
         else:
             print("Starting coordinates are:  X:", x , " -   Y: ", y)
-        print(rows)
         moving()
+
     except:
         print("failed to format and create x / y inital location")
 
 
 
-def moving(x_initial,y_initial,rows):  # Will Calculate and create the
+def moving(x_initial,y_initial,rows):  # Will add up all the directions into 
+
     print("Running Formatter")
     global current_lococation,file_location, x,y
+    
     try:
-        for direction in rows:
-            if direction == "N":
+        for i in rows:
+            i = str(i.upper())
+            if i == "N":
                 y += 1
-            elif direction == "S":
+            elif i == "S":
                 y -= 1
-            elif direction == "E":
+            elif i == "E":
                 x += 1
-            elif direction == "W":
+            elif i == "W":
                 x -= 1
             else:
-                print(direction , " not used")
-            if not (0 <= y < grid_size and 0 <= x < grid_size):
-                print("drone lost at Co-ordinates:", x, " - ", y)
+                print(i , " not used")
+            if y > grid_size or x > grid_size or y <= 0 or x <= 0:
+                print("drone out of bounds Co-ordinates:", x, " - ", y)
             else:
                 current_lococation.add((x, y))
-            grid_generator(x_initial,y_initial,rows)
+            grid_generator(x_initial,y_initial,rows,current_lococation)
     except:
         print("Faile to process file")
 
 
 
 
-def grid_generator(x_initial,y_initial,rows): #once rows and initial position is calculated, a grid can be generated
+def grid_generator(x_initial,y_initial,rows_list,visted_locations): #once rows and initial position is calculated, a grid can be generated
     print("Printing Grid")
     error = False
     coords = []
