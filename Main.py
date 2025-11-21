d028e257f79aa0523427ab8da1edbe8073b63410
vlad <138448988+gearupp@users.noreply.github.com>
2025-11-21 10:06:41 +0000

    added more to File Formater / processor. Fixed fileinput needs to be string before Regex can work on it.

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import re
 
 grid_size = 11
-global file_location, current_loc
+global file_location, current_lococation
 
 text1 = './Route001.txt'
 text03 = './Route003.txt'
@@ -26,7 +26,7 @@ def fileinput():
     while True:
         global file_location
         file_location = input("Enter the full path to the route file (or type STOP to exit): ")
-        if re.search("(.txt)",fileinput ):
+        if re.search("(.txt)",str(fileinput) ):
             file_formating() # Will run after a path with .txt is provided
             exit
         elif file_location.lower().strip() == "stop":
@@ -39,6 +39,40 @@ fileinput()
 
 def file_formating():
     print("Running Formatter")
+    global current_lococation,x,y,file_location
+    try:
+       with open(file_location, 'r') as file:
+           row = [row for row in file if row.strip()]
+           x = int(row[0])
+           y = int(row[1])
+           current_lococation = set()
+           print(f" Starting coordinates are: ({x}, {y})")
+
+           for direction in lines[2:]:
+               direction = direction.upper()
+               if direction == "N":
+                   y += 1
+               elif direction == "S":
+                   y -= 1
+               elif direction == "E":
+                   x += 1
+               elif direction == "W":
+                   x -= 1
+               else:
+                   print(direction , " not used")
+               if not (0 <= y < grid_size and 0 <= x < grid_size):
+                   
+                   print(f"drone lost at Co-ordinates: ({x}, {y})")
+                   break
+               else:
+                   curentLocation.add((x, y))
+           fileinput()
+    except:
+        print("Faile to process file")
+
+print(current_lococation)
+print(file_location)
 
 
-    
+
+fileinput()
